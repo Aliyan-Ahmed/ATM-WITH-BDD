26cab0464d3afd6f1fe9de3191e965f76a117fcd
Aliyan Ahmed <aliyanahmed@gmail.com>
2023-01-08 20:52:31 +0500

    8-JAN-2023
    ATM WITH BDD WORK

---
+++ ATM/ATM/Features/AmountWithdrawal.feature
@@ -0,0 +1,10 @@
+﻿Feature: AmountWithdrawal
+	Current Amount and Withdraw
+
+@mytag
+Scenario: Current amount after Withdraw
+	Given Amount in Account
+	And Enter the choice
+	And Withdraw the amount
+	When Validate the Pin	 
+	Then Your current amount
---
+++ ATM/ATM/Steps/AmountWithdrawalSteps.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace ATM.Steps
+{
+    [Binding]
+    public class AmountWithdrawalSteps
+    {
+        ATM atm = new ATM();
+        Param parame = new Param();
+
+        [Given(@"Amount in Account")]
+        public void GivenAmountInAccount()
+        {
+            parame.amount = 50000;
+        }
+        
+        [Given(@"Enter the choice")]
+        public void GivenEnterTheChoice()
+        {
+            parame.choice = 2;
+        }
+        
+        [Given(@"Withdraw the amount")]
+        public void GivenWithdrawTheAmount()
+        {
+            parame.withdraw = 10000;
+        }
+        
+        [When(@"Validate the Pin")]
+        public void WhenValidateThePin()
+        {
+            parame.pin = 123;
+        }
+        
+        [Then(@"Your current amount")]
+        public void ThenYourCurrentAmount()
+        {
+            Assert.AreEqual(atm.BankTransaction(parame), "Your current amount now is 40000");
+        }
+    }
+}
